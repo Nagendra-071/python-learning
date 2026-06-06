b34cab554e7f69cb4dbbeb0cc7ee43a5b5539486
Ayush Singh <r5radhasingh515@gmail.com>
2026-06-06 14:06:52 +0530

    Use of Shutil and os library

---
+++ day87.py
@@ -0,0 +1,18 @@
+import shutil
+import os
+
+#copy a file 
+shutil.copy("day46/Tutorial 2/oslist.py","oslist2.py")
+
+#copy a folder
+shutil.copytree("day46", "nelist")
+
+
+#move a file to other location
+shutil.move("day46/tutorial 2/renam.py","newfile")
+
+#delete a folder
+shutil.rmtree("day46/tutorial 2")
+
+#file remove
+os.remove("day46/tutorial 2/renam.py")
