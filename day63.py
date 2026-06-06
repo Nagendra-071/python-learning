e0098851ee376f4a4c4b15e253e2eca8791dd4aa
Ayush Singh <r5radhasingh515@gmail.com>
2026-06-06 14:26:49 +0530

    Day 63: Exercise 6 - Library Management System

--- day63.py
+++ day63.py
@@ -12,11 +12,12 @@ row1.add("Story of tintin")
 row1.add("1984")
 
 print(f"Row 1 consit of {row1.books} these books") 
-print("Total No . of books in row 1 : ",row1.count)
+print("Total No . of books in row 1 : ",
+      row1.count)
 
 row2=Library()
 row2.add("Story  of Bhagat Singh")        
 row2.add("LOC")
 
 print(f"Row 2 consit of {row2.books} these books") 
-print("Total No . of books in row 2 : ",row2.count)
+print("Total No . of books in row 2 : ",row2.count) 
